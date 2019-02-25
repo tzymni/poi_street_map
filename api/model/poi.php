0ea9b9039b2cfb0dd6aee9369a5af3345ef01173
Tomasz Zymni <tomasz23fl@gmail.com>
2019-02-25 19:29:22 +0100

    pierwszy commit

---
+++ api/model/poi.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of poi
+ *
+ * @author tzymni
+ */
+class poi {
+    //put your code here
+}
